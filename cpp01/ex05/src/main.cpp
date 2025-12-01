59b74c7fbd2ca01b4532024a4b125db5a5155656
Maxim Kleverov <mklevero@student.hive.fi>
2025-12-01 15:48:44 +0200

    05 done

---
+++ cpp01/ex05/src/main.cpp
@@ -0,0 +1,12 @@
+#include "Harl.hpp"
+#include <iostream>
+
+
+int main(int ac, char **av){
+	if(ac != 2){
+		std::cerr << "try again" << std::endl;
+		return (1);
+	}
+	Harl harl;
+	harl.complain(av[1]);
+}
