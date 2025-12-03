d54cb24d8f4bab0c71abee1dfd3ef37edef2de2e
Maxim Kleverov <mklevero@student.hive.fi>
2025-12-03 14:57:33 +0200

    fixes

---
+++ cpp02/ex02/inc/Fixed.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+
+class Fixed
+{
+  private:
+	int _value;
+	static const int _fractionalBits = 8;
+
+  public:
+	Fixed();
+	Fixed(const int iNum);
+	Fixed(const float fNum);
+	~Fixed();
+	Fixed(const Fixed &other);
+	Fixed &operator=(const Fixed &other);
+
+	Fixed operator+(const Fixed &other);
+	Fixed operator-(const Fixed &other);
+	Fixed operator*(const Fixed &other);
+	Fixed operator/(const Fixed &other);
+
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+
+	float toFloat(void) const;
+	int toInt(void) const;
+};
+
+std::ostream &operator<<(std::ostream &out, const Fixed &value);
